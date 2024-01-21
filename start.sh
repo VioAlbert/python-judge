24e7cb06cd97482d96391e981a21e45c41fd848a
Vio Albert Ferdinand <vioalbert08@gmail.com>
2024-01-21 17:17:16 +0700

    chore: refactor start script

--- start.sh
+++ start.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/bash
 
-param=()
-
-for var in "$@"; do
-  if ! [[ $var == "-"* ]]; then
-    var="$(realpath $var)"
-  fi
-  param+=($var)
-done
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-python $SCRIPT_DIR/main.py ${param[*]}
+python $SCRIPT_DIR/main.py "$@"
